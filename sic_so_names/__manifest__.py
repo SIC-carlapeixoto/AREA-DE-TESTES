121f9a21a9a617aab063bb9453f7da82f4c1d941
SIC-miguelbrito <114583878+SIC-miguelbrito@users.noreply.github.com>
2023-01-04 11:45:20 +0000

    Test Module

---
+++ sic_so_names/__manifest__.py
@@ -0,0 +1,34 @@
+
+# -*- coding: utf-8 -*-
+{
+    'name': "SIC PO Name",
+
+    'summary': """
+        SIC PO Name""",
+
+    'description': """
+        SIC PO Name
+    """,
+
+    'author': "SmartImproveConsulting",
+    'website': "https://www.smartimprove.pt",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+
+    "depends": ["base","sale"],
+    # always loaded
+    'data': [
+
+    ],    
+
+    # only loaded in demonstration mode
+    'demo': [
+        #'demo/demo.xml',
+    ],
+}
---
+++ sic_so_names/models/sale_order.py
@@ -0,0 +1,14 @@
+from odoo import api, models, fields, _
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+
+    @api.multi
+    def write(self, values):
+        if 'name' in values.keys():
+            purchase_ids = self.env['purchase.order'].search([('origin', '=', self.name)])
+            for po in purchase_ids:
+                po.origin = values['name']
+        super(SaleOrder, self).write()
+        
